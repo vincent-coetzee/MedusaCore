a00cae1c0576d735021dcfc2bdfbe00c82d40b71
Vincent Coetzee <vincent.coetzee@icloud.com>
2023-12-05 20:37:09 +0200

    Added Types

---
+++ Sources/MedusaCore/Types.swift
@@ -0,0 +1,54 @@
+//
+//  File.swift
+//  
+//
+//  Created by Vincent Coetzee on 05/12/2023.
+//
+
+import Foundation
+
+public struct Medusa
+    {
+    public typealias            Address = Medusa.Integer64
+    public typealias               Atom = Medusa.Integer64
+    public typealias            Boolean = Swift.Bool
+    public typealias               Byte = Swift.UInt8
+    public typealias           Checksum = UInt64
+//    public typealias Enumeration = MOPEnumeration
+    public typealias            Float64 = Swift.Double
+    public typealias            Float32 = Swift.Float
+    public typealias            Float16 = Swift.Int16
+    public typealias          Integer64 = Swift.Int
+    public typealias          Integer32 = Swift.Int32
+    public typealias          Integer16 = Swift.Int16
+    public typealias        MagicNumber = UInt64
+    public typealias           ObjectID = Swift.UInt64
+    public typealias            Pointer = Integer64
+    public typealias         RawPointer = UnsafeMutableRawPointer
+    public typealias             String = Swift.String
+    public typealias      UnicodeScalar = Unicode.Scalar
+    public typealias         Unsigned64 = UInt64
+    public typealias         Unsigned32 = UInt32
+    public typealias         Unsigned16 = UInt16
+    }
+
+public typealias                Address = Medusa.Address
+public typealias                   Atom = Medusa.Atom
+public typealias                Boolean = Medusa.Boolean
+public typealias                   Byte = Medusa.Byte
+public typealias               Checksum = Medusa.Checksum
+public typealias                Float64 = Medusa.Float64
+public typealias                Float32 = Medusa.Float32
+public typealias                Float16 = Medusa.Float16
+public typealias              Integer64 = Medusa.Integer64
+public typealias              Integer32 = Medusa.Integer32
+public typealias              Integer16 = Medusa.Integer16
+public typealias            MagicNumber = Medusa.MagicNumber
+public typealias               ObjectID = Medusa.ObjectID
+public typealias                Pointer = Medusa.Pointer
+public typealias             RawPointer = Medusa.RawPointer
+public typealias                 String = Medusa.String
+public typealias          UnicodeScalar = Medusa.UnicodeScalar
+public typealias             Unsigned64 = Medusa.Unsigned64
+public typealias             Unsigned32 = Medusa.Unsigned32
+public typealias             Unsigned16 = Medusa.Unsigned16
